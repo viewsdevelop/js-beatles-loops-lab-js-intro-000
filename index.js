216ed5a99bce8c28e2123811f20faa60406de73c
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-11 15:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musicPairings = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    musicPairings.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return musicPairings;
+}
+
+function johnLennonFacts(facts) {
+  let exclaimedFacts = []
+
+  let i = 0;
+
+  while (i < facts.length) {
+    exclaimedFacts.push(`${facts[i]}!!!`);
+    i += 1;
+  }
+
+  return exclaimedFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var loveStatements = [];
+
+  let i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+  do {
+
+  }
+}

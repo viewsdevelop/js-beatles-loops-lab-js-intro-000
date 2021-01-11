606713893306b2153422d24acc7426cf791ffc3f
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-11 15:39:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musicPairings = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    musicPairings.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return musicPairings;
+}
+
+function johnLennonFacts(facts) {
+  let exclaimedFacts = []
+
+  let i = 0;
+
+  while (i < facts.length) {
+    exclaimedFacts.push(`${facts[i]}!!!`);
+    i += 1;
+  }
+
+  return exclaimedFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var loveStatements = [];
+
+  function incrementVariable() {
+    number = number + 1;
+    return number;
+  }
+
+  do {
+    loveStatements.push("I love the Beatles!");
+  } while (incrementVariable() < 15);
+
+  return loveStatements;
+}
